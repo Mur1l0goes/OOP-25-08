14d7fac0b43601b852ed537ae741ca0419d2782a
Mur1l0goes <murilogoes005@gmail.com>
2025-09-30 00:29:48 +0000

    feat: add projeto

--- sobrescrita/Program.cs
+++ sobrescrita/Program.cs
@@ -8,3 +8,8 @@ CultureInfo.CurrentCulture = new CultureInfo("en-GB");
 
 Cartao cartaoBase = new Cartao();
 cartaoBase.Debitar(100);
+
+Console.WriteLine("-- Cartão de Débito --");
+
+Cartao cartaoDebito = new CartaoDebito();
+cartaoDebito.Debitar(100);
