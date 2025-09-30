cf61e311173f759b3bbc6bad3a2e9fec2bbff5e2
Mur1l0goes <murilogoes005@gmail.com>
2025-09-30 00:33:36 +0000

    feat: add projeto

--- sobrescrita/CartaoDebito.cs
+++ sobrescrita/CartaoDebito.cs
@@ -2,6 +2,10 @@ public class CartaoDebito : Cartao
 {
     public override void Debitar(decimal valor)
     {
+        Console.WriteLine("Chamando classe base");
+        base.Debitar(valor);
+
+        Console.WriteLine("Chamando sobrescrita");
         Console.WriteLine($"Compra {valor:C} realizado no cartão de débito.");
     }
 }
