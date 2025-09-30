7b6d79baead699fc432ef529b5a8084ff553b76e
Mur1l0goes <murilogoes005@gmail.com>
2025-09-30 00:17:07 +0000

    feat: add projeto

---
+++ sobrescrita/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World!");
+
+CultureInfo.CurrentCulture = new CultureInfo("en-GB");
+
+Cartao cartaoBase = new Cartao();
+cartaoBase.Debitar(100);
